0271a96aed59e0452af28dc8b2a1cd412898dba6
lee <78707392+lee-asc@users.noreply.github.com>
2025-05-06 19:35:31 +0100

    Update A_E_Binomial_Model.py

--- misc_code/A_E_Binomial_Model.py
+++ misc_code/A_E_Binomial_Model.py
@@ -1,13 +1,5 @@
-# --- TUTORIAL 5              -------------------------------------------------
-#
-#     BINOMIAL TREE                                                                     
-# 
-# --- Define Libraries          -----------------------------------------------
-
 import math as m
 import numpy as np
-
-# --- Define Input Data       -------------------------------------------------
 
 cp = +1     # +1/-1 for Call/Put
 ae = -1     # +1/-1 for American/European
@@ -20,32 +12,21 @@ sigma	= 0.3
 
 n = 100
 
-# --- Calculation Other Values   ----------------------------------------------
-
 dt = T / n              # <- h
 u = m.exp(sigma * m.sqrt(dt))
 d = 1 / u
 p = (m.exp(r * dt) - d) / (u - d)    
     
-# --- Define Result Matrix    -------------------------------------------------
-
 opt_val = np.zeros((n+1, n+1))
 S_val = np.zeros((n+1, n+1))
 
-
-
-    
-    # Generate the Underlying Stock Price ----------
  
 S_val[0,0] = S
 for i in range(1,n+1):
     S_val[i,0] = S_val[i-1,0] * u
     for j in range(1,i+1):
         S_val[i,j] = S_val[i-1,j-1] * d
-            
-  
-  #Backward recursion for option price -----
-    
+                
 for j in range(n+1):
     opt_val[n,j] = max(0, cp*( S_val[n,j]-K )) #payoff at T
     
